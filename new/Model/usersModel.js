3d4c50ca5b57e6ac2b52cb4864ca81bde3898ff1
Vijay Tahkur <dev@aron123>
2023-08-08 13:06:21 +0530

    ok

--- new/Model/usersModel.js
+++ new/Model/usersModel.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
     introduction: { type: String },
     image: {
       type: String,
