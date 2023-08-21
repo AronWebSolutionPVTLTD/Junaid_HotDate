724894197649c6e8769e6f0e0939081c674faa33
Aronwebsolution <44797871+AronWebSolutionPVTLTD@users.noreply.github.com>
2023-08-21 14:35:35 +0530

    Update userController.js

--- latestbackend18/Controller/userController.js
+++ latestbackend18/Controller/userController.js
@@ -24,11 +24,11 @@ module.exports = {
         }
         const exist = await userModel.findOne({ email });
         if (exist) {
-          return res.status(400).send("User already exists");
+          return res.status(200).send("User already exists");
         }
         const username_exist = await userModel.findOne({ username: username });
         if (username_exist) {
-          return res.status(400).send("Username already exist");
+          return res.status(200).send("Username already exist");
         }
         if (confirmpassword !== password) {
           return res.status(400).send("Password doesm't match");
@@ -44,64 +44,94 @@ module.exports = {
           return res.status(400).send("Failed to create user");
         } else {
           const verificationLink = `${process.env.FRONTEND_URL}/verified/${data._id}`;
+
           let emailHtml = `
-          <!doctype html>
-          <html lang="en-US">
+          <!DOCTYPE html>
+          <html lang="en">
           <head>
-              <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-              <title>Email Verification</title>
-              <meta name="description" content="Email Verification Template.">
-              <style type="text/css">
-                  a:hover { text-decoration: underline !important; }
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <style>
+                  *{margin:0;padding: 0;box-sizing: border-box;}
+                  .top_bar {
+                      height: 30px;
+                      background:linear-gradient(46deg, #F79220 55.15%, #F94A2B 82%);
+                  }
+                  .email_temp_inner {
+                      max-width: 720px;
+                      margin: 40px auto 0;
+                  }
+                  img{
+                      max-width: 100%;
+                  }
+                  .logo_wrap {
+                      max-width: 130px;
+                      margin: 50px auto;
+                  }
+                  .confirm_email_title {
+              text-align: center;
+              font-size: 20px;
+              margin-bottom: 20px;
+          }
+          .verification_btn {
+              display: block;
+              width: 100%;
+              max-width: 200px;
+              margin: 0 auto 150px;
+              text-align: center;
+              background: #00bf63;
+              color: #f7f7f7;
+              font-size: 18px;
+              padding: 12px 10px;
+              border-radius: 7px;
+              cursor: pointer;
+          }
+          .confirm_email_footer {
+              margin-bottom: 50px;
+          }
+          
+          
+          .footer_title {
+              font-size: 20px;
+              font-weight: 600;
+              margin-bottom: 10px;
+          }
+          .footer_right {
+              display: grid;
+              gap: 6px;
+          }
+          
+          .footer_right a {
+              color: #1357b3;
+              text-decoration: none;
+          }
               </style>
           </head>
-          <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #F2F3F8;" leftmargin="0">
-              <!-- 100% body table -->
-              <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#F2F3F8"
-                  style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700%7COpen+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-                  <tr>
-                      <td>
-                          <table style="background-color: #F2F3F8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                              align="center" cellpadding="0" cellspacing="0">
-                              <tr>
-                                  <td style="height: 80px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                  <td style="height: 20px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                  <td>
-                                      <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                          style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                          <tr>
-                                              <td style="height: 40px;">&nbsp;</td>
-                                          </tr>
-                                          <tr>
-                                              <td style="padding: 0 35px;">
-                                                  <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Email Verification</h1>
-                                                  <span style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #CECECE; width:100px;"></span>
-                                                  <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">Thank you for signing up. Please verify your email address by clicking the button below.</p>
-                                                  <a  href="${verificationLink}"
-                                                      style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Verify Email</a>
-                                              </td>
-                                          </tr>
-                                          <tr>
-                                              <td style="height: 40px;">&nbsp;</td>
-                                          </tr>
-                                      </table>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td style="height: 20px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                  <td style="height: 80px;">&nbsp;</td>
-                              </tr>
-                          </table>
-                      </td>
-                  </tr>
-              </table>
-              <!-- /100% body table -->
+          <body>
+              <div class="email_temp_wrapper">
+                  <div class="top_bar"></div>
+                  <div class="email_temp_inner">
+                      <div class="logo_wrap">
+                          <img src="http://localhost:3000/landingPage/images/landing-logo.png" alt="Logo" />
+                      </div>
+                      <div class="confirm_email_wrap">
+                          <p class="confirm_email_title">Please confirm your email address by clicking the 'Verify Email' button.</p>
+                          <a class="verification_btn" href=${verificationLink}>Verify Email</a>
+                          <div class="confirm_email_footer">
+                              <p>Sincerely,</p>
+                              <p>Your Kaizen Globe staff</p>
+                          </div>
+                      </div>
+                      <div class="email_temp_footer">
+                          <p class="footer_title">Contact Us</p>
+                          <div class="footer_right">
+                              <p>Kaizen Globe</p>
+                              <p><a href="tel:+91-1234567890">+91-1234567890</a></p>
+                              <p><a href="mailto:test@gmail.com">test@gmail.com</a></p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </body>
           </html>
           `;
