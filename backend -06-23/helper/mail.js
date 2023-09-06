13736147d213f228c517f09427982524a8b3f37a
Vijay Tahkur <dev@aron123>
2023-09-06 19:23:43 +0530

    message

---
+++ backend -06-23/helper/mail.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+const Mailsend=(req,res,data)=>{
+    var transporter = nodemailer.createTransport({
+        host: "smtp-relay.brevo.com", // Replace with your SMTP server host
+        port: 465, // Replace with the appropriate port
+        secure: true,
+        auth: {
+          user: process.env.Nodemailer_id,
+          pass: process.env.Nodemailer_pass,
+        },
+      });
+      var mailOptions = data;
+      transporter.sendMail(mailOptions, function (error, result) {
+        if (error) {
+          console.log("Email error sent: " + JSON.stringify(error));
+          return res.status(400).json(error);
+        } else {
+          console.log("Email result sent: " + JSON.stringify(result));
+          return res.status(200).json("send mail successfully");
+        }
+      });
+}
+module.exports=Mailsend;
